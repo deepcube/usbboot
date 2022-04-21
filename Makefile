deepcube/usbboot

commit 45c7237d9dc22704e9e950966b7d1a73ec185006

Fixup for recent firmware inclusion changes (#34)
* Fix check against NULL for a static var actually looking for an empty c string.

* Update .gitignore for code change moving msd firmware to static vars.

* Fix support for OS X and *BSD after adding static vars for msd firmware.

main.c
@@ -8,13 +8,18 @@
 #include "msd/bootcode.h"
 #include "msd/start.h"
 
+/* Assume BSD without native fmemopen() if doesn't seem to be glibc */
+#if defined(__APPLE__) || !defined(_GNU_SOURCE) || !defined(_POSIX_C_SOURCE) || _POSIX_C_SOURCE < 200809L
+#include "fmemopen.c" // BSD fmemopen() compat in terms of funopen()
+#endif
+
 int signed_boot = 0;
 int verbose = 0;
 int loop = 0;
 int overlay = 0;
 long delay = 500;
 char * directory = NULL;
-char pathname[18];
+char pathname[18] = {0};
 uint8_t targetPortNo = 99;
 
 int out_ep;
@@ -413,7 +418,7 @@ FILE * check_file(char * dir, char *fname)
 	// Check directory first then /usr/share/rpiboot
 	if(dir)
 	{
-		if(overlay&&(pathname != NULL))
+		if(overlay&&(pathname[0] != 0))
 		{
 			strcpy(path, dir);
 			strcat(path, "/");

.gitignore
@@ -1,1 +1,2 @@
 rpiboot
+bin2c

fmemopen.c
@@ -1,0 +1,107 @@
+//
+// Copyright 2011-2014 NimbusKit
+// Originally ported from https://github.com/ingenuitas/python-tesseract/blob/master/fmemopen.c
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+
+struct fmem {
+  size_t pos;
+  size_t size;
+  char *buffer;
+};
+typedef struct fmem fmem_t;
+
+static int readfn(void *handler, char *buf, int size) {
+  fmem_t *mem = handler;
+  size_t available = mem->size - mem->pos;
+
+  if (size > (int)available) {
+    size = available;
+  }
+  memcpy(buf, mem->buffer + mem->pos, sizeof(char) * size);
+  mem->pos += size;
+
+  return size;
+}
+
+static int writefn(void *handler, const char *buf, int size) {
+  fmem_t *mem = handler;
+  size_t available = mem->size - mem->pos;
+
+  if (size > (int)available) {
+    size = available;
+  }
+  memcpy(mem->buffer + mem->pos, buf, sizeof(char) * size);
+  mem->pos += size;
+
+  return size;
+}
+
+static fpos_t seekfn(void *handler, fpos_t offset, int whence) {
+  size_t pos;
+  fmem_t *mem = handler;
+
+  switch (whence) {
+    case SEEK_SET: {
+      if (offset >= 0) {
+        pos = (size_t)offset;
+      } else {
+        pos = 0;
+      }
+      break;
+    }
+    case SEEK_CUR: {
+      if (offset >= 0 || (size_t)(-offset) <= mem->pos) {
+        pos = mem->pos + (size_t)offset;
+      } else {
+        pos = 0;
+      }
+      break;
+    }
+    case SEEK_END: pos = mem->size + (size_t)offset; break;
+    default: return -1;
+  }
+
+  if (pos > mem->size) {
+    return -1;
+  }
+
+  mem->pos = pos;
+  return (fpos_t)pos;
+}
+
+static int closefn(void *handler) {
+  free(handler);
+  return 0;
+}
+
+FILE *fmemopen(void *buf, size_t size, const char *mode) {
+  #pragma unused(mode)
+  // This data is released on fclose.
+  fmem_t* mem = (fmem_t *) malloc(sizeof(fmem_t));
+
+  // Zero-out the structure.
+  memset(mem, 0, sizeof(fmem_t));
+
+  mem->size = size;
+  mem->buffer = buf;
+
+  // funopen's man page: https://developer.apple.com/library/mac/#documentation/Darwin/Reference/ManPages/man3/funopen.3.html
+  return funopen(mem, readfn, writefn, seekfn, closefn);
+}

msd/.gitignore
@@ -1,0 +1,1 @@
+*.h

commit 692dad174477502f584272a56f4370272ceba238

configure: add configure script to handle fmemopen
Previously two commits added a fallback fmemopen for OS X/BSD that
didn't support fmemopen, then fixed some of the ifdef logic around it.
Unforunately the logic was backwards, as libc implementations do not
define _POSIX_C_SOURCE in order to advertise features.  Instead the
user defines _POSIX_C_SOURCE to control which definitions are
available.

At first glance _POSIX_VERSION could be tested to see if fmemopen
is available, but this only solves half the problem.  The fallback
implementation uses then non-standard BSD specific funopen.  This means
that even if _POSIX_VERSION is tested, there needs to be a way to
test for the existence of funopen.

Solve the problem with a short configure script that checks for
fmemopen and funopen.  This will work independent of which libc and
version are used.

Fixes: 45c7237 (Fixup for recent firmware inclusion changes (#34))
Fixes: 17f6b01 (Fix cross-platform building)

Makefile
@@ -1,7 +1,21 @@
+# Change settings in config.mk or as arguments to make. E.g.
+#
+#     make CFLAGS='-Wall -Wextra -Werror'
+
 PKG_VER=$(shell grep rpiboot debian/changelog | head -n1 | sed 's/.*(\(.*\)).*/\1/g')
 GIT_VER=$(shell git rev-parse HEAD 2>/dev/null | cut -c1-8 || echo "")
+
+include config.mk
+
+CFLAGS_ALL = \
+	$(CFLAGS_FTM) \
+	$(CFLAGS_NEED_HAVE) \
+	-DGIT_VER='"$(GIT_VER)"'\
+	-DPKG_VER='"$(PKG_VER)"' \
+	$(CFLAGS)
+
 rpiboot: main.c msd/bootcode.h msd/start.h msd/bootcode4.h msd/start4.h
-	$(CC) -Wall -Wextra -g -o $@ $< `pkg-config --cflags --libs libusb-1.0` -DGIT_VER="\"$(GIT_VER)\"" -DPKG_VER="\"$(PKG_VER)\""
+	$(CC) $(CFLAGS_ALL) -Wall -Wextra -g -o $@ $< `pkg-config --cflags --libs libusb-1.0`
 
 %.h: %.bin ./bin2c
 	./bin2c $< $@
@@ -29,6 +43,6 @@ uninstall:
 	rmdir --ignore-fail-on-non-empty /usr/share/rpiboot/
 
 clean:
-	rm -f rpiboot msd/*.h bin2c
+	rm -f rpiboot msd/*.h bin2c config.mk
 
 .PHONY: uninstall clean

configure
@@ -1,0 +1,53 @@
+#!/bin/sh
+
+# Settings via environment variables. E.g.
+#
+#     CC=gcc CFLAGS=-g ./configure
+
+[ "$CC" ] || CC=cc
+CFLAGS_FTM='-D_POSIX_C_SOURCE=200809L -D_DEFAULT_SOURCE -D_BSD_SOURCE'
+CFLAGS_ALL="$CFLAGS_FTM $CFLAGS"
+
+tmpc=conf-tmp.c
+trap 'rm "$tmpc"' EXIT INT QUIT TERM HUP
+
+printf %s 'checking whether libc implements fmemopen... '
+cat > "$tmpc" << 'EOF'
+#include <stdio.h>
+FILE *(*p)(void *, size_t, const char *) = fmemopen;
+EOF
+if $CC $CFLAGS_ALL -Werror -o /dev/null -c "$tmpc" >/dev/null 2>&1; then
+	echo yes
+else
+	echo no
+	CFLAGS_FMEMOPEN=-DNEED_FMEMOPEN
+fi
+
+printf %s 'checking whether libc implements funopen... '
+cat > "$tmpc" << 'EOF'
+#include <stdio.h>
+FILE *(*p)(
+	const void *,
+	int (*)(void *, char *, int),
+	int (*)(void *, const char *, int),
+	fpos_t (*)(void *, fpos_t, int),
+	int (*)(void *)
+) = funopen;
+EOF
+if $CC $CFLAGS_ALL -Werror -o /dev/null -c "$tmpc" >/dev/null 2>&1; then
+	echo yes
+	CFLAGS_FUNOPEN=-DHAVE_FUNOPEN
+else
+	echo no
+fi
+
+printf %s 'creating config.mk... '
+cat > config.mk << EOF
+# This file was generated by configure.
+# Any changes made will be lost if configure is re-run.
+CC = $CC
+CFLAGS = $CFLAGS
+CFLAGS_FTM = $CFLAGS_FTM
+CFLAGS_NEED_HAVE = $CFLAGS_FMEMOPEN $CFLAGS_FUNOPEN
+EOF
+echo done

main.c
@@ -10,9 +10,12 @@
 #include "msd/bootcode4.h"
 #include "msd/start4.h"
 
-/* Assume BSD without native fmemopen() if doesn't seem to be glibc */
-#if defined(__APPLE__) || (!defined(_GNU_SOURCE) && (!defined(_POSIX_C_SOURCE) || _POSIX_C_SOURCE < 200809L))
-#include "fmemopen.c" // BSD fmemopen() compat in terms of funopen()
+#ifdef NEED_FMEMOPEN
+	#ifdef HAVE_FUNOPEN
+		#include "fmemopen.c"
+	#else
+		#error Need fmemopen or funopen, or new fmemopen fallback.
+	#endif
 #endif
 
 int signed_boot = 0;